4d7e57f3122c6ddf76494826dcdd8ab5d61487eb
tungnguyen <minhtung.uit@gmail.com>
2025-08-27 23:10:29 +0700

    Up api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\CategoryController;
 
 Route::apiResource('books', BookController::class);
+Route::apiResource('authors', AuthorController::class);
+Route::apiResource('categories', CategoryController::class);
 
 ?>
